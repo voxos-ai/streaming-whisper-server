e1c55f5dc82fd19ddc715cf474f6b45d42294f3f
anshjoseph <ranshjosrph@gmail.com>
2024-05-20 16:07:06 +0530

    update readme and change project creation code

--- WhisperLive/__main__.py
+++ WhisperLive/__main__.py
@@ -2,48 +2,50 @@ import argparse
 import os
 
 simple_server = """
-from CustomWhisper import TranscriptionServer
+from WhisperLive import TranscriptionServer
 import argparse
+import os
 
 if __name__ == "__main__":
-    server = TranscriptionServer(use_vad=True,denoise=False)
+    
+    ASRs = [i.name for i in os.scandir("./ASR") if i.is_dir]
+    with open("./hotwords",'r') as file:
+        hotwords = [i for i in file.readlines() if (i.strip()) != 0]
+    if len(hotwords) <= 0:
+        hotwords = None
+    server = TranscriptionServer(use_vad=True,denoise=False,hotwords=hotwords,model_list=ASRs)
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', '-p',
                         type=int,
                         default=9090,
                         help="Websocket port to run the server on.")
-    parser.add_argument('--faster_whisper_custom_model_path', '-fw',
-                        type=str, default=None,
-                        help="Custom Faster Whisper Model")
     args = parser.parse_args()
     server.run(
         "0.0.0.0",
-        port=args.port,
-        backend="faster_whisper",
-        faster_whisper_custom_model_path=args.faster_whisper_custom_model_path
-    )
+        port=args.port)
 """
 denoise_server = """
-from CustomWhisper import TranscriptionServer
+from WhisperLive import TranscriptionServer
 import argparse
+import os
 
 if __name__ == "__main__":
-    server = TranscriptionServer(use_vad=True,denoise=True)
+    
+    ASRs = [i.name for i in os.scandir("./ASR") if i.is_dir]
+    with open("./hotwords",'r') as file:
+        hotwords = [i for i in file.readlines() if (i.strip()) != 0]
+    if len(hotwords) <= 0:
+        hotwords = None
+    server = TranscriptionServer(use_vad=True,denoise=True,hotwords=hotwords,model_list=ASRs)
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', '-p',
                         type=int,
                         default=9090,
                         help="Websocket port to run the server on.")
-    parser.add_argument('--faster_whisper_custom_model_path', '-fw',
-                        type=str, default=None,
-                        help="Custom Faster Whisper Model")
     args = parser.parse_args()
     server.run(
         "0.0.0.0",
-        port=args.port,
-        backend="faster_whisper",
-        faster_whisper_custom_model_path=args.faster_whisper_custom_model_path
-    )
+        port=args.port)
 """
 parser = argparse.ArgumentParser()
 parser.add_argument('--create_project', '-c',
@@ -59,6 +61,8 @@ if __name__ == "__main__":
     os.mkdir(f"{args.create_project}/ASR")
     os.mkdir(f"{args.create_project}/NoiseWeights")
     os.mkdir(f"{args.create_project}/VAD")
+    with open(f"{args.create_project}/hotwords",'w') as file:
+        pass
     with open(f"{args.create_project}/Server.py",'w') as file:
         if args.denoise:
             file.write(denoise_server)
@@ -66,6 +70,6 @@ if __name__ == "__main__":
             file.write(denoise_server)    
     # os.system(f"ct2-transformers-converter --model {args.whisper_model_name} --copy_files preprocessor_config.json --output_dir {out_dir} --quantization float16")
     os.system(f"wget https://dl.fbaipublicfiles.com/adiyoss/denoiser/dns48-11decc9d8e3f0998.th -O {args.create_project}/NoiseWeights/model.th")
-    os.system(f"wget https://github.com/anshjoseph/WhisperCustom/raw/master/example/SimpleExample/VAD/silero_vad.onnx -O {args.create_project}/VAD/silero_vad.onnx")
+    os.system(f"wget https://github.com/SYSTRAN/faster-whisper/raw/master/faster_whisper/assets/silero_vad.onnx -O {args.create_project}/VAD/silero_vad.onnx")
     print("HERE IS BASIC SETUP OF FOLDER TO RUN THIS MODULE IN SERVER SIDE")
     print(f"use this command to load ASR: ct2-transformers-converter --model openai/whisper-tiny --copy_files preprocessor_config.json --output_dir /ASR/whisper-tiny --quantization float16")
--- readme.md
+++ readme.md
@@ -1,5 +1,15 @@
-# CustomWhisper
-this package is combination whisper live and denoise packages, 
+# WhisperLive
+This module integrates Facebook's denoising technology with OpenAI's Whisper ASR (Automatic Speech Recognition) system to reduce noise in input audio streams, improving transcription accuracy and reducing hallucinations. The module supports multi-threading and can load various Whisper models, including language-specific models from Hugging Face, making it versatile for different transcription needs. The server-client architecture allows users to leverage Whisper's capabilities in their daily tasks.
+Features
+
+- Noise Reduction: Utilizes Facebook's denoising algorithm to clean input audio.
+- Whisper Integration: Leverages Whisper's robust ASR capabilities for accurate  transcription.
+- Multi-Model Support: Easily load different Whisper models, including language-specific models from Hugging Face.
+- Server-Client Architecture: Runs Whisper on a server, with a client program for interfacing.
+- Real-Time Processing: Supports real-time audio processing for immediate feedback.
+- Multi-Threading: Handles multiple requests simultaneously for efficient processing.
+- Hot-word: you can specify the hot word in `hotword` file inside the server folder by which it can focus on particular words accoring to you needs
+  
 
 ## Install Instruction
 ```shell
@@ -14,14 +24,16 @@ note: dont worry about version conflict
 ```shell
 python3 -m CustomWhisper -c <project name> -dn <True,False (for denoise active or deactive)>
 ```
+project have three folder ASR, NoiseWeights, VAD and two file hotwords and Server.py you have to make sure you put model in ASR folder, the server code automatically detect model from ASR folder so please make sure of that, if client send wrong model name then , first model is selected automatically
 ## Add ASR models
 ```shell
 ct2-transformers-converter --model <model: openai/whisper-tiny> --copy_files preprocessor_config.json --output_dir <output_dir: ASR/whisper_tiny_ct> --quantization float16
 ```
+note: add it on ASR folder
 ## Run server
 ```
 > cd my-project
 > ls
-ASR  NoiseWeights  Server.py  VAD
+ASR  NoiseWeights hotwords Server.py  VAD
 > python3 Server.py -p 9000
 ```
