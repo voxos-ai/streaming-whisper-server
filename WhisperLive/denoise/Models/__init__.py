272423978c699d26e93c9939e37015262782cfab
anshjoseph <ranshjosrph@gmail.com>
2024-08-10 10:50:03 +0530

    resolve small fix

--- WhisperLive/denoise/Models/__init__.py
+++ WhisperLive/denoise/Models/__init__.py
@@ -1,12 +1,10 @@
 from .base import Denoise
 from .deepfilternet_denoise import DeepFilterNetDenoise
 from .facebook_denoise import FaceBookDenoise
-from .speechbrain_denoise import SpeechBrainDenoise
 # from typing import Dict
 
 
 Denoisers ={
     "FaceBookDenoise": FaceBookDenoise,
     "DeepFilterNetDenoise": DeepFilterNetDenoise,
-    "SpeechBrainDenoise": SpeechBrainDenoise
 }
